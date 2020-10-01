4d8ffd96ba55607823284efc74eac54f51878d72
Dadanon <mar4ello19922@gmail.com>
2020-10-01 08:31:01 +0300

    Initial from other lesson

--- api/views.py
+++ api/views.py
@@ -1,8 +1,7 @@
-from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404, get_list_or_404
+from django.shortcuts import get_object_or_404
 
 from polls.models import Question, Choice
 from .serializers import (
